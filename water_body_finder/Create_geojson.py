203dd42e20b51989f2e2ccf6232096dc2bc300b5
Dylan <dylangeldenhuys1@gmail.com>
2020-04-05 16:53:12 +0200

    added functions

---
+++ water_body_finder/Create_geojson.py
@@ -0,0 +1,26 @@
+# Author: Dylan Geldenhuys
+
+from shapely.wkt import loads
+from shapely.geometry import mapping
+from geojson import Point, Feature, FeatureCollection, dump
+import rasterio
+from shapely.geometry import LineString
+
+
+
+def pixelcoord_to_geocoord(pixel_coordinate):
+    #pixel coordinate must be tuple type
+    # define raterio_object outside the function, rasterio_object = rasterio.open(image)
+    return(rasterio_object.transform * pixel_coordinate)
+
+
+def create_geojson(ordered_list, geoTif):
+    rasterio_object = rasterio.open(geoTif)
+    features = []
+    for l in ordered_list[:4]:
+        tuple_of_tuples = tuple(tuple(x) for x in l)
+        Lstring = LineString(list(map(pixelcoord_to_geocoord,tuple_of_tuples)))
+        features.append(Feature(geometry=Lstring))
+    feature_collection = FeatureCollection(features)
+    with open('myfile.geojson', 'w') as f:
+        dump(feature_collection, f)
---
+++ water_body_finder/create_polygon.py
@@ -0,0 +1,54 @@
+#Authors: Simon, Dylan
+
+def get_boundary(result):
+    edges = []
+    for j in range(result.shape[0] - 1):
+        for i in range(result.shape[1] - 1):
+            if (result[j, i] < 175):
+                if (result[j, i] != result[j, i + 1] or result[j, i] != result[j, i - 1] or result[j, i] != result[j + 1, i] or result[j, i] != result[j - 1, i]):
+                    edges.append([j,i])
+    return(edges)
+
+def get_boundary2(mask):
+    edges = []
+    for j in range(mask.shape[0]):
+        for i in range(mask.shape[1]):
+            if (mask[j, i] < 175):
+                if (mask[j, i + 1] > 175
+                or mask[j, i - 1] > 175
+                or mask[j + 1, i] > 175
+                or mask[j - 1, i] > 175):
+                    edges.append([j, i])
+    return(edges)
+
+
+def order_points_simply(complex_points):
+    complex_points_ls = list(complex_points)
+    simple_lists = [[]]
+    polygon_index = 0
+    point = complex_points_ls[0]
+    point_index = 0
+    while len(complex_points_ls) > 1:
+        point_found = False
+        #print(len(complex_points_ls))
+        for i in range(len(complex_points_ls)):
+            if (abs(complex_points_ls[i][0] - point[0]) <= 1 and abs(complex_points_ls[i][1] - point[1]) <= 1):
+                if (point[0],point[1]) != (complex_points_ls[i][0],complex_points_ls[i][1]):
+                    simple_lists[polygon_index].append(list(point))
+                    point = complex_points_ls[i]
+                    complex_points_ls.pop(point_index)
+                    if point_index < i:
+                        point_index = i-1
+                    else:
+                        point_index = i
+                    point_found = True
+                    break
+        if point_found == False:
+            simple_lists[polygon_index].append(list(point))
+            complex_points_ls.pop(point_index)
+            point = complex_points_ls[0]
+            point_index = 0
+            polygon_index += 1
+            simple_lists.append([])
+    simple_lists[polygon_index].append(list(point))        
+    return simple_lists
