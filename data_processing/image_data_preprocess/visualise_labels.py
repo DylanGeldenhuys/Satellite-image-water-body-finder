11bcb9fc8badd148128e9136a5acc5f75b35b1f1
simongasson <simongasson@gmail.com>
2020-04-26 18:18:04 +0200

    label_visualisation

--- data_processing/image_data_preprocess/visualise_labels.py
+++ data_processing/image_data_preprocess/visualise_labels.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from rasterio.plot import reshape_as_image
 import rasterio
 from PIL import Image
+from skimage.filters.rank import entropy
+from skimage.morphology import disk
 import os
 
 
@@ -12,7 +14,7 @@ image_data_directory = Path(
 label_data_directory = Path(
     "D:/WaterBodyExtraction/WaterPolyData/label_data")
 
-filename = os.listdir(image_data_directory)[1]
+filename = os.listdir(image_data_directory)[4]
 
 # load files
 raster_image_data = rasterio.open(
@@ -25,11 +27,14 @@ label_data = np.load(label_data_directory.joinpath(
 label_data_alpha = np.zeros(
     (label_data.shape[0], label_data.shape[1], 4), dtype=np.uint8)
 
-label_data_alpha[label_data == 0] = [255, 0, 0, 150]
+label_data_alpha[label_data == 0] = [255, 0, 0, 50]
 label_data_alpha[label_data == 1] = [0, 0, 0, 0]
 Image.fromarray(label_data_alpha)
 
-plt.figure()
-plt.imshow(image_data)
-plt.imshow(label_data_alpha)
+fig, (ax0, ax1) = plt.subplots(ncols=2, sharex=True, sharey=True)
+
+ax0.imshow(image_data)
+ax0.imshow(label_data_alpha)
+
+ax1.imshow(image_data)
 plt.show()
