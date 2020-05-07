351155b0a9d64bca37b4f39ce43d856ab3b1cf0f
Dylan <dylangeldenhuys1@gmail.com>
2020-05-07 21:54:15 +0200

    WIP

--- data_processing/image_data_preprocess/visualise_labels.py
+++ data_processing/image_data_preprocess/visualise_labels.py
@@ -14,28 +14,28 @@ image_data_directory = Path(
 label_data_directory = Path(
     "/media/ds/New Volume/Waterbody_Project/new_labels")
 
-filename = '2628BB01.tif' #os.listdir(image_data_directory)[4]
+filename = '3220DA15.tif' #os.listdir(image_data_directory)[4]
 
 # load files
 raster_image_data = rasterio.open(
     image_data_directory.joinpath(filename)).read()
 image_data = reshape_as_image(raster_image_data)
 
-label_data = np.load(label_data_directory.joinpath(
-    filename.replace("tif", "npy")))
+#label_data = np.load(label_data_directory.joinpath(
+#    filename.replace("tif", "npy")))
 
-label_data_alpha = np.zeros(
-    (label_data.shape[0], label_data.shape[1], 4), dtype=np.uint8)
+#label_data_alpha = np.zeros(
+#    (label_data.shape[0], label_data.shape[1], 4), dtype=np.uint8)
 
-label_data_alpha[label_data == 0] = [255, 17, 0, 30]
+#label_data_alpha[label_data == 0] = [255, 17, 0, 30]
 
-label_data_alpha[label_data == 1] = [0, 0, 0, 0]
-Image.fromarray(label_data_alpha)
+#label_data_alpha[label_data == 1] = [0, 0, 0, 0]
+#Image.fromarray(label_data_alpha)
 
 fig, (ax0) = plt.subplots(ncols=1, sharex=True, sharey=True)
 
 ax0.imshow(image_data)
-ax0.imshow(label_data_alpha)
+#ax0.imshow(label_data_alpha)
 
 #ax1.imshow(image_data)
 
