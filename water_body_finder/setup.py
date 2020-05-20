be2a4c666d536c93bd8cbda1b2f591bbff517612
Dylan <dylangeldenhuys1@gmail.com>
2020-05-20 18:10:23 +0200

    wip

--- water_body_finder/setup.py
+++ water_body_finder/setup.py
@@ -1,17 +1,23 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
 
 setuptools.setup(
     name="example-pkg-YOUR-USERNAME-HERE",  # Replace with your own username
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
+    #description="A small example package",
+    #long_description=long_description,
+    #long_description_content_type="text/markdown",
+    install_requires=requirements,
+    #packages=setuptools.find_packages(),
+    
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
