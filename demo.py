b736ca21fbcbc53b3ddad2524e8a898a285f05c1
simon-gasson <simon@haefelesoftware.com>
2020-05-23 21:13:44 +0200

    Fixed training

--- demo.py
+++ demo.py
@@ -1,16 +1,20 @@
-from water_body_finder import find_waterbodies, create_training, train_model, save_prediction_mask
+from water_body_finder import find_waterbodies, create_training, train_model, save_prediction_mask, save_polygons
 
 if __name__ == '__main__':
 
-    #create_training(image_input_dir = "D:/WaterBodyExtraction/WaterPolyData/image_data",                              
-    #                geo_data_dir = "D:/WaterBodyExtraction/WaterPolyData/geo_data/v2",output_dir =  "D:/Demo/output")
+    # create_training(image_input_dir="D:/WaterBodyExtraction/WaterPolyData/image_data",
+    #                 geo_data_dir="D:/WaterBodyExtraction/WaterPolyData/geo_data/v2", output_dir="D:/Demo/output", percentage_sample=2)
 
-    #train_model(output_dir ="D:/Demo/output", training_dir = "D:/Demo/output/training",
-    #            version=0, sub_version=1)
+    # train_model(output_dir="D:/Demo/output", training_dir="D:/Demo/output/training",
+    #            version=0, sub_version=0)
 
     save_prediction_mask(
-       image_input_dir = "D:/WaterBodyExtraction/WaterPolyData/image_data", output_dir ="D:/Demo/output", 
-       rfc_dir = "D:/Demo/output/rfc", version = "0_1", padding = 50, window_size = 3000, resolution = 3)
+        image_input_dir="D:/WaterBodyExtraction/WaterPolyData/image_data", output_dir="D:/Demo/output",
+        rfc_dir="D:/Demo/output/rfc", version="0_0", padding=50, window_size=3000, resolution=3)
 
-    find_waterbodies(image_input_dir = "D:/WaterBodyExtraction/WaterPolyData/image_data",
-                     output_dir = "D:/Demo/output", padding=1200, window_size=3000)
+    # save_polygons(
+    #    image_input_dir="D:/WaterBodyExtraction/WaterPolyData/image_data", output_dir="D:/Demo/output",
+    #    rfc_dir="D:/Demo/output/rfc", version="0", padding=50, window_size=3000, resolution=3)
+
+    # find_waterbodies(image_input_dir="D:/WaterBodyExtraction/WaterPolyData/image_data",
+    #                 output_dir="D:/Demo/output", padding=1200, window_size=3000)
--- water_body_finder/test.py
+++ water_body_finder/test.py
@@ -12,7 +12,7 @@ import multiprocessing as mp
 
 from .feature_extraction import extract_features
 from .utilities import get_boundary, order_points, save_geojson, load_window, correct_point_offset, is_touching
-from .predict import predict_mask_full
+from .predict import Model
 
 
 def save_prediction_mask(image_input_dir, output_dir, rfc_dir, version, padding, window_size, resolution):
@@ -29,8 +29,32 @@ def save_prediction_mask(image_input_dir, output_dir, rfc_dir, version, padding,
     pool = mp.Pool()
     for filename in filenames[0:1]:
         img_src = image_input_path.joinpath(filename)
-        prediction = predict_mask_full(
-            rfc, img_src, padding, window_size, resolution, pool)
+        model = Model(rfc, img_src, padding, window_size, resolution, pool)
+        prediction = model.predict_mask_full(pool)
+        np.save(output_path.joinpath(
+            filename.replace('tif', 'npy')), prediction)
+        print("Completed: {}".format(filename.replace('.geojson', '')))
+
+    pool.close()
+    pool.join()
+
+
+def save_polygons(image_input_dir, output_dir, rfc_dir, version, padding, window_size, resolution):
+    image_input_path = Path(image_input_dir)
+    rfc_path = Path(rfc_dir).joinpath("rfc_{}.p".format(version))
+    output_path = Path(output_dir).joinpath(
+        "polygons").joinpath("version_{}".format(version))
+    output_path.mkdir(parents=True, exist_ok=True)
+
+    f = open(rfc_path, 'rb')
+    rfc = pickle.load(f)
+
+    filenames = os.listdir(image_input_dir)
+    pool = mp.Pool()
+    for filename in filenames[1:2]:
+        img_src = image_input_path.joinpath(filename)
+        model = Model(rfc, img_src, padding, window_size, resolution, pool)
+        prediction = model.predict_polygons(pool)
         np.save(output_path.joinpath(
             filename.replace('tif', 'npy')), prediction)
         print("Completed: {}".format(filename.replace('.geojson', '')))
